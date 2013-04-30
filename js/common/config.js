590588e90fc84762f02ecfea09957bd744051456
switer <guankaishe@gmail.com>
2013-04-30 11:18:51 +0800

    add common dir

---
+++ js/common/config.js
@@ -0,0 +1,39 @@
+var localConfig = {
+	languages : {
+		"zh" : {
+			toolBar_save 			: "保存",
+			toolBar_import 			: "导入幻灯片文件",
+			toolBar_openFileSystem 	: "打开保存文件保存目录",
+			toolBar_preview 		: "预览",
+			toolBar_setAnimation 	: "更改幻灯片过渡动画",
+			toolBar_addVideo 		: "添加视频(暂时只支持Mmp4格式)",
+			toolBar_addImage 		: "插入一张图片",
+			toolBar_addText 		: "插入文本输入框",
+			toolBar_addCode 		: "插入代码编辑框",
+			anim_slider 			: "幻灯片",
+			anim_sliderZoom 		: "方法幻灯片",
+			anim_rotateLeft 		: "向左旋转",
+			anim_rotateRight 		: "向右旋转",
+			anim_reverse 			: "垂直翻转",
+			anim_label				: "过渡动画"
+		},
+		'en' : {
+			toolBar_save 			: "save file",
+			toolBar_import 			: "import file",
+			toolBar_openFileSystem 	: "open storage filesystem",
+			toolBar_preview 		: "current presentation preview",
+			toolBar_setAnimation 	: "set frame transition animation",
+			toolBar_addVideo 		: "add a video element",
+			toolBar_addImage 		: "add an image element",
+			toolBar_addText 		: "add a text-box element",
+			toolBar_addCode 		: "add a high-light editor element",
+			toolBar_changeScale 	: "change current presentation screen size",
+			anim_slider 			: "slider",
+			anim_sliderZoom 		: "slider zoom",
+			anim_rotateLeft 		: "rotate left",
+			anim_rotateRight 		: "rotate right",
+			anim_reverse 			: "reverse",
+			anim_label				: "transition"
+		}
+	}
+}
